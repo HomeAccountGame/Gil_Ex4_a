78875b22de1027ef5be94ef456af2737173af1aa
Gil Vexberg <74468196+gilivex@users.noreply.github.com>
2023-04-18 20:53:25 +0300

    Scripitis

---
+++ Scripts/2-spawners/ClickSpawner.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/**
+ * This component spawns the given object whenever the player clicks a given key.
+ */
+public class ClickSpawner : MonoBehaviour
+{
+    [SerializeField] protected InputAction spawnAction = new InputAction(type: InputActionType.Button);
+    [SerializeField] protected GameObject prefabToSpawn;
+    [SerializeField] protected Vector3 velocityOfSpawnedObject;
+    [SerializeField] protected float cooldownTime = 0.5f;  // cooldown time between shots
+    private float lastSpawnTime;  // the time the last shot was fired
+
+    void OnEnable()
+    {
+        spawnAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        spawnAction.Disable();
+    }
+
+    protected virtual GameObject spawnObject()
+    {
+        Debug.Log("Spawning a new object");
+
+        // Check if enough time has passed since the last shot
+        if (Time.time - lastSpawnTime < cooldownTime)
+        {
+            return null;  // don't spawn a new object yet
+        }
+
+        // Step 1: spawn the new object.
+        Vector3 positionOfSpawnedObject = transform.position;  // span at the containing object position.
+        Quaternion rotationOfSpawnedObject = Quaternion.identity;  // no rotation.
+        GameObject newObject = Instantiate(prefabToSpawn, positionOfSpawnedObject, rotationOfSpawnedObject);
+
+        // Step 2: modify the velocity of the new object.
+        Mover newObjectMover = newObject.GetComponent<Mover>();
+        if (newObjectMover)
+        {
+            newObjectMover.SetVelocity(velocityOfSpawnedObject);
+        }
+
+        // Update last spawn time
+        lastSpawnTime = Time.time;
+
+        return newObject;
+    }
+
+    private void Update()
+    {
+        if (spawnAction.WasPressedThisFrame())
+        {
+            spawnObject();
+        }
+    }
+}
